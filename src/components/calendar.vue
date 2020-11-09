b31d5b3c113ec7691382704781813ec608d52012
Peter <p.j.m.smit@student.utwente.nl>
2020-11-09 10:13:02 +0100

    made calendar more mobile-friendly

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row class="fill-height">
+  <v-row justify="center" align="center" class="fill-height">
     <v-col>
       <!-- Start of the top bar. Includes today, previous and forward buttons and current month. -->
       <v-sheet height="64">
@@ -32,7 +32,7 @@
         </v-toolbar>
       </v-sheet>
       <!-- End of the top bar -->
-      <v-sheet height="600">
+      <v-sheet class="mx-auto" height="600">
         <!-- The actual calendar -->
         <v-calendar
             ref="calendar"
@@ -162,7 +162,6 @@ export default {
   methods: {
     getEvents(month) {
       if (!this.monthsCollected.includes(month)) {
-        console.log("getting events for " + month);
         this.monthsCollected.push(month)
         setTimeout(() => this.monthsLoading++, 500);
 
@@ -184,8 +183,7 @@ export default {
               )
               this.events.push(...res);
             })
-            .catch(error => {
-              console.log(error)
+            .catch(() => {
               this.snackbar = true
             })
             .then(() => this.monthsLoading--);
@@ -298,7 +296,6 @@ export default {
       }).format(date).replace(',', '');
     },
     eventsByDate(date) {
-      console.log(date.toLocaleString());
       return this.events.filter((event) => date.toLocaleString() === event.date);
     }
   }
