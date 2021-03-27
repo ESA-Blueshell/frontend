7ad600195384e9f6121225a9614ea64205a41c00
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-03-27 10:58:42 +0100

    fixed docuuuments

--- src/views/Documents.vue
+++ src/views/Documents.vue
@@ -1,33 +1,30 @@
 <template>
-  <v-main>
+  <v-main style="background: black">
     <top-banner title="Documents"></top-banner>
-    <div class="black" style="height: 100%;width: 100%;position: fixed;">
-    <div>
-      <div class="mx-auto" style="max-width: 800px">
-        <p class="body-1 green--text" style="font-family: monospace !important">
-          >ssh pi@blueshell-server <br>
-          >... <br>
-          >authentication required <br>
-          <i>>login: shitshell <br>
+    <div class="mx-auto my-10" style="max-width: 800px">
+      <p class="body-1 green--text" style="font-family: monospace !important">
+        >ssh pi@blueshell-server <br>
+        >... <br>
+        >authentication required <br>
+        <i>>login: shitshell <br>
           >password: shitshell </i> <br>
-          >invalid login <br>
-          <i>>login: peter <br>
+        >invalid login <br>
+        <i>>login: peter <br>
           >password: peter2020 </i> <br>
-          >invalid login <br>
-          <i>>login: andrei <br>
+        >invalid login <br>
+        <i>>login: andrei <br>
           >password: andrei </i> <br>
-          >login successful <br>
-          >cd ../blueshell <br>
-          >ls <br>
-          >/blueshell-frontend /blueshell-api /blueshell-keylogger /documents <br>
-          >cd /documents <br>
-          >ls <br>
-          >Meeting_11252020.pdf <br>
-          >scp shitshell@shitpostcie Meeting_11252020.pdf <br>
-          <a href="https://esa-blueshell.nl/api/download/Meeting_11252020.pdf" class="text-decoration-none" target="_blank"> ACCESS HERE </a> <br>
-        </p>
-      </div>
-    </div>
+        >login successful <br>
+        >cd ../blueshell <br>
+        >ls <br>
+        >/blueshell-frontend /blueshell-api /blueshell-keylogger /documents <br>
+        >cd /documents <br>
+        >ls <br>
+        >Meeting_11252020.pdf <br>
+        >scp shitshell@shitpostcie Meeting_11252020.pdf <br>
+        <a href="https://esa-blueshell.nl/api/download/Meeting_11252020.pdf"
+           class="text-decoration-none" target="_blank"> ACCESS HERE </a> <br>
+      </p>
     </div>
   </v-main>
 </template>
