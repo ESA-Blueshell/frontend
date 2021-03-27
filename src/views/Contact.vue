fa12523c37b154a14cf2d3c88721d8ef27654698
s1870084 <a.raureanu@student.utwente.nl>
2021-03-27 22:38:33 +0100

    fuck

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,7 +5,11 @@
 
     <div class="mx-3">
       <div class="mx-auto my-10" style="max-width: 800px">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d13875480.657673547!2d51.916651361161954!3d31.89114714990479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x4010befa0386e3a7%3A0x5cfc88a76177780b!2sBenis%2C%20Oost-Azerbeidzjan%2C%20Iran!3m2!1d38.2090726!2d45.7283577!4m5!1s0x396e150dcafedabb%3A0xe181edd3201759a9!2sBagina%2C%20Rajasthan%2C%20India!3m2!1d26.1480148!2d76.12257269999999!5e0!3m2!1snl!2snl!4v1616876263774!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <p class="body-1">
+
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d24329044.59420074!2d-120.43473522951693!3d41.895995244079145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x88471d243c38d9c3%3A0xec734e7e6ae648be!2sPee%20Pee%20Creek%2C%20Ohio%2C%20Verenigde%20Staten!3m2!1d39.156857599999995!2d-83.0911979!4m5!1s0x54906508f3cbe15b%3A0x91eb43cf8a7c9305!2sPoo%20Poo%20Point%20Trail%2C%20Washington%2098027%2C%20Verenigde%20Staten!3m2!1d47.5064332!2d-122.005957!5e0!3m2!1snl!2snl!4v1616879605989!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d13875480.657673547!2d51.916651361161954!3d31.89114714990479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x4010befa0386e3a7%3A0x5cfc88a76177780b!2sBenis%2C%20Oost-Azerbeidzjan%2C%20Iran!3m2!1d38.2090726!2d45.7283577!4m5!1s0x396e150dcafedabb%3A0xe181edd3201759a9!2sBagina%2C%20Rajasthan%2C%20India!3m2!1d26.1480148!2d76.12257269999999!5e0!3m2!1snl!2snl!4v1616876263774!5m2!1snl!2snl" width="600" height="450" style="border:0; margin: auto;" allowfullscreen="" loading="lazy"></iframe>
       </div>
     </div>
   </v-main>
