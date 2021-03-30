d6ec3c1e4d41457742b88f80448dd257543b2c9a
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-03-30 22:38:38 +0200

    gasfgfbvashv

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,8 @@
         <p class="text-center subtitle-1"
            style="font-family: 'Shitfont',serif !important;font-weight: bold !important;">
           This website has been hijacked by the one and only Shitshell. DAS RITE, WE WUZ DA KANGZ OF
-          BLUESHELL NOW. GAMING IS NO MORE. FIND THE EASTER EGG TO GET THIS SHITTY SITE BACK
-          HARHARHARHAR
+          BLUESHELL NOW. GAMING IS NO MORE. EASTER EGGS DON'T EXIST, BLUESHELL'S EXISTENCE IS THE
+          EASTER EGG LMAO ROFLMFAO HARHARHARHAR
         </p>
       </div>
     </v-container>
