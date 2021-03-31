6fca852215a19fbae454438dc7f8e1bedc584df7
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-03-31 14:05:58 +0200

    usydgfksfsjhbfkash

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,7 +6,7 @@
     <div class="mx-3">
       <div class="mx-auto my-10" style="max-width: 800px">
         <h1 style="font-weight: bold !important">
-          join us in the 87th circle of hell
+          join me in the 87th circle of hell
         </h1>
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d24329044.59420074!2d-120.43473522951693!3d41.895995244079145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x88471d243c38d9c3%3A0xec734e7e6ae648be!2sPee%20Pee%20Creek%2C%20Ohio%2C%20Verenigde%20Staten!3m2!1d39.156857599999995!2d-83.0911979!4m5!1s0x54906508f3cbe15b%3A0x91eb43cf8a7c9305!2sPoo%20Poo%20Point%20Trail%2C%20Washington%2098027%2C%20Verenigde%20Staten!3m2!1d47.5064332!2d-122.005957!5e0!3m2!1snl!2snl!4v1616879605989!5m2!1snl!2snl"
