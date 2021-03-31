3f8217c9ae8490797e2b49e8587631f1fd7bf0ff
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-04-01 00:53:15 +0200

    sjhadkfbgsjb

--- src/App.vue
+++ src/App.vue
@@ -154,8 +154,8 @@
         <v-icon>mdi-twitter</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn href="https://esa-blueshell.nl:28315" target="_blank" style="font-family: 'Shitfont',serif !important;">
-        <v-icon>take me back to the normal site</v-icon>
+      <v-btn href="https://esa-blueshell.nl:28315" target="_blank" >
+        take me back to the normal site
       </v-btn>
       <div class="white--text">SITECIE GANG &copy; {{ new Date().getFullYear() }}</div>
     </v-footer>
