5c5d522acd77c15e96acd1a75251240363e61697
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-10-10 22:36:52 +0200

    more housestyle fixes

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,13 @@
 <template>
   <v-main>
-    <div class="body-1 mt-16 text-center">
-      <v-img class="mx-auto" :src="require('../assets/shellyooooooooooooohhhhhhhhhhh.png')" max-width="800"/>
-      <h1 class="mt-10">Uh oh, we made a fucky wucky! Please click <a href="/" class="text-decoration-none">here</a> to go back to main page!</h1>
+    <div class="body-1 mx-auto mt-16 text-center" style="max-width: 800px;">
+      <v-img class="mx-auto" :src="require('../assets/shellyooooooooooooohhhhhhhhhhh.png')"
+             max-width="800"/>
+      <h3 class="mt-10">
+        Uh oh, we made a fucky wucky! Pwease don't report this incident to board otherwise they will
+        whip us again. Or do if you're hate sitecie (but then we hate you too >:(
+      </h3>
+      <h1 class="mt-5">Click <a href="/" class="text-decoration-none">here</a> to go back to main page!</h1>
     </div>
   </v-main>
 </template>
