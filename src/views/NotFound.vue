d05ee7fa1021f7f7b03aac613346de4af4c6353d
Peter Smit <p.j.m.smit@student.utwente.nl>
2021-10-10 21:45:53 +0200

    updated 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,8 @@
 <template>
   <v-main>
     <div class="body-1 mt-16 text-center">
-      <h1 style="margin-top: 350px">Uh oh, we made a fucky wucky! Please click <a href="/" class="text-decoration-none">here</a> to go back to main page!</h1>
+      <v-img class="mx-auto" :src="require('../assets/shellyooooooooooooohhhhhhhhhhh.png')" max-width="800"/>
+      <h1 class="mt-10">Uh oh, we made a fucky wucky! Please click <a href="/" class="text-decoration-none">here</a> to go back to main page!</h1>
     </div>
   </v-main>
 </template>
