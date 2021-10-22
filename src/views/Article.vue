1a430d453b78d00c06a70bb716cb4b6ba77d3637
Peter Smit <43498358+Petersmit27@users.noreply.github.com>
2021-10-22 20:12:22 +0200

    added share buttons to news article

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -9,6 +9,14 @@
       <h5>By <b>{{ news.creatorUsername }}</b>,
         {{ news.postedAt ? news.postedAt.slice(0, 10) : '' }}
       </h5>
+      <v-btn icon
+             :href="'http://twitter.com/share?text='+news.title+'&url='+this.thisURL()+'&hashtags='+news.newsType">
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn icon
+             :href="'https://www.facebook.com/sharer/sharer.php?u='+this.thisURL()+'&t='+news.title">
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
     </div>
     <v-snackbar v-model="snackbar" timeout="10000">
       Uh oh, looks like we can't connect to the server :/ <br>
@@ -42,6 +50,11 @@ export default {
         .get('news/' + this.$route.params.id)
         .then(response => this.news = response.data)
         .catch(() => this.snackbar = true)
+  },
+  methods: {
+    thisURL() {
+      return document.URL
+    }
   }
 }
 </script>
