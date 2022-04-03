4c5882b5a17f03590306ee23dcab7560a233df47
Peter Smit <43498358+Petersmit27@users.noreply.github.com>
2022-04-03 11:39:21 +0200

    Changed event-form such that startTime must always be before endTime

--- src/components/event-form.vue
+++ src/components/event-form.vue
@@ -1,55 +1,55 @@
 <template>
-  <v-form v-model="valid" ref="form">
+  <v-form ref="form" v-model="valid">
     <v-container>
 
       <v-row>
-        <v-col xl="8" lg="8" md="8" sm="8" cols="12">
+        <v-col cols="12" lg="8" md="8" sm="8" xl="8">
           <v-text-field
+              ref="title"
               v-model="event.title"
               :rules="[v => !!v || 'Title is required']"
-              @input="$emit('title', event.title)"
               label="Event name"
               required
-              ref="title"/>
+              @input="$emit('title', event.title)"/>
         </v-col>
-        <v-col xl="4" lg="4" md="4" sm="4" cols="12">
+        <v-col cols="12" lg="4" md="4" sm="4" xl="4">
           <v-text-field
+              ref="location"
               v-model="event.location"
               :rules="[v => (!!v || !event.visible) || 'Location is required for public events']"
               label="Location"
-              required
-              ref="location"/>
+              required/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-textarea
+              ref="description"
               v-model="event.description"
               :rules="[v => !!v || 'Description is required']"
               label="Description"
-              required
               outlined
-              ref="description"/>
+              required/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-text-field
+              ref="memberPrice"
               v-model="event.memberPrice"
               :rules="priceRules.concat([v => (!!v || !event.visible) || 'Price is required for public events'])"
               label="Price for members"
               prepend-icon="mdi-currency-eur"
-              required
-              ref="memberPrice"/>
+              required/>
         </v-col>
         <v-col>
           <v-text-field
+              ref="publicPrice"
               v-model="event.publicPrice"
               :rules="priceRules.concat([v => (!!v || !event.visible) || 'Price is required for public events'])"
               label="Price for non-members"
               prepend-icon="mdi-currency-eur"
-              required
-              ref="publicPrice"/>
+              required/>
         </v-col>
       </v-row>
       <v-row>
@@ -66,7 +66,6 @@
         <v-col>
           <v-checkbox
               v-model="event.visible"
-              @click="$refs.form.validate()"
               :rules="[v => !!v || 'For now, non-public events are not supported, cry about it']"
               label="Make public"/>
 
@@ -75,57 +74,59 @@
       <v-row>
         <v-col>
           <v-text-field
-              type="date"
               v-model="event.startDate"
               :rules="[v => (!!v || !event.visible) || 'Start date is required for public events']"
+              label="Start date"
               prepend-icon="mdi-calendar"
-              label="Start date"/>
+              type="date"/>
         </v-col>
         <v-col>
           <v-text-field
-              type="time"
               v-model="event.startTime"
               :rules="[v => (!!v || !event.visible) || 'Start time is required for public events']"
+              label="Start time"
               prepend-icon="mdi-clock"
-              label="Start time"/>
+              type="time"/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-text-field
-              type="date"
               v-model="endDateDisplay"
-              :rules="[v => (!!v || !event.visible) || 'End date is required for public events']"
               :disabled="event.endDateSame"
+              :rules="[v => (!!v || !event.visible) || 'End date is required for public events',
+                       v => (!!v && new Date(event.startDate) <= new Date(endDateDisplay)) || 'End date cannot be earlier than start date']"
+              label="End date"
               prepend-icon="mdi-calendar"
-              label="End date"/>
+              type="date"/>
         </v-col>
         <v-col>
           <v-text-field
-              type="time"
               v-model="event.endTime"
-              :rules="[v => (!!v || !event.visible) || 'End time is required for public events']"
+              :rules="[v => (!!v || !event.visible) || 'End time is required for public events',
+              v => (!!v && new Date(`${event.startDate} ${event.startTime}`) < new Date(`${endDateDisplay} ${v}`)) || 'Event must end after it starts']"
+              label="End time"
               prepend-icon="mdi-clock"
-              label="End time"/>
+              type="time"/>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-select
+              v-model="event.committeeId"
+              :disabled="!committees"
               :items="committees"
+              :rules="[v => (!!v || !event.visible) || 'A representative committee is required for public events']"
               item-text="name"
               item-value="id"
-              :disabled="!committees"
-              v-model="event.committeeId"
-              :rules="[v => (!!v || !event.visible) || 'A representative committee is required for public events']"
               label="Representative committee"/>
         </v-col>
         <v-col>
           <v-file-input
               v-model="event.image"
               accept="image/*"
-              show-size
               label="Promo image"
+              show-size
           />
         </v-col>
       </v-row>
@@ -134,28 +135,28 @@
 
           <v-checkbox
               v-model="event.signUp"
-              @click="event.enableSignUpForm = event.signUp&&event.enableSignUpForm"
-              label="Enable sign-up"/>
+              label="Enable sign-up"
+              @click="event.enableSignUpForm = event.signUp&&event.enableSignUpForm"/>
         </v-col>
         <v-col>
           <v-checkbox
               v-model="event.enableSignUpForm"
-              @click="event.signUp = event.signUp||event.enableSignUpForm"
-              label="Enable sign-up form"/>
+              label="Enable sign-up form"
+              @click="event.signUp = event.signUp||event.enableSignUpForm"/>
         </v-col>
       </v-row>
       <v-row v-if="event.enableSignUpForm">
         <v-col>
-          <create-sign-up-form :form="event.signUpForm" ref="signUpForm"/>
+          <create-sign-up-form ref="signUpForm" :form="event.signUpForm"/>
         </v-col>
       </v-row>
     </v-container>
 
 
     <v-btn
-        class="mt-4"
-        block
         :disabled="!valid"
+        block
+        class="mt-4"
         color="primary"
         @click="submit()">
       Submit event
@@ -234,7 +235,9 @@ export default {
         this.event.endDate = this.event.startDate
       }
 
-      this.$emit('submit', this.event);
+      if (this.$refs.form.validate()) {
+        this.$emit('submit', this.event);
+      }
     }
   },
   mounted() {
