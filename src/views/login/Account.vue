10d039993106e06584d167d7c3adf1e01c569b70
Peter Smit <p.j.m.smit@student.utwente.nl>
2022-09-05 21:53:30 +0200

    on account page, added validation for some fields and removed the "edit mode" layout

--- src/views/login/Account.vue
+++ src/views/login/Account.vue
@@ -4,73 +4,79 @@
     <div class="mx-3">
       <div class="mx-auto my-10" style="max-width: 800px">
         <div v-if="this.accountData">
-          <v-row align="end" align-self="end"> <!-- ?????????? why does it not align right?????????????? -->
-            <v-col cols="auto" v-if="!editing">
-              <v-btn icon @click="edit">
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-            </v-col>
-            <v-col cols="auto" v-if="editing">
-              <v-btn icon @click="cancel">
-                <v-icon>mdi-close</v-icon>
-              </v-btn>
-            </v-col>
-            <v-col cols="auto" v-if="editing">
-              <v-btn icon @click="save">
-                <v-icon>mdi-content-save</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
+          <v-form ref="form" v-model="valid">
+            <v-row align="end">
+              <v-col cols="auto" align-self="end">
+                <v-tooltip top>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon @click="save" :disabled="!valid"
+                           v-bind="attrs" v-on="on">
+                      <v-icon>mdi-content-save</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Save changes</span>
+                </v-tooltip>
+              </v-col>
+            </v-row>
 
-          <v-text-field :readonly="editing?null:true" label="Username" v-model="accountData.username"/>
-          <v-row>
-            <v-col cols="6">
-              <v-text-field :readonly="editing?null:true" label="First name" v-model="accountData.firstName"/>
-            </v-col>
-            <v-col cols="2">
-              <v-text-field :readonly="editing?null:true" label="Prefix" v-model="accountData.prefix"/>
-            </v-col>
-            <v-col cols="4">
-              <v-text-field :readonly="editing?null:true" label="Last name" v-model="accountData.lastName"/>
-            </v-col>
-          </v-row>
-          <v-text-field :readonly="editing?null:true" label="initials" v-model="accountData.initials"/>
-          <v-text-field :readonly="editing?null:true" label="address" v-model="accountData.address"/>
-          <v-text-field :readonly="editing?null:true" label="houseNumber" v-model="accountData.houseNumber"/>
-          <v-text-field :readonly="editing?null:true" label="postalCode" v-model="accountData.postalCode"/>
-          <v-text-field :readonly="editing?null:true" label="city" v-model="accountData.city"/>
-          <v-text-field :readonly="editing?null:true" label="phoneNumber" v-model="accountData.phoneNumber"/>
-          <v-text-field :readonly="editing?null:true" label="email" v-model="accountData.email"/>
-          <v-text-field :readonly="editing?null:true" label="studentNumber" v-model="accountData.studentNumber"/>
-          <v-text-field :readonly="editing?null:true" label="dateOfBirth" v-model="accountData.dateOfBirth"/>
-          <v-text-field :readonly="editing?null:true" label="discord" v-model="accountData.discord"/>
-          <v-text-field :readonly="editing?null:true" label="steamid" v-model="accountData.steamid"/>
-          <v-row>
-            <v-col>
-              <v-checkbox :readonly="editing?null:true" label="Subscribe to newsletter"
-                          v-model="accountData.newsletter"/>
-            </v-col>
-            <v-col>
-              <v-checkbox :readonly="editing?null:true" label="Give consent for your photo to be taken at events"
-                          v-model="accountData.photoConsent"/>
-            </v-col>
-          </v-row>
+            <v-text-field disabled label="Username" v-model="accountData.username"/>
+            <v-row>
+              <v-col cols="2">
+                <v-text-field disabled label="initials" v-model="accountData.initials"/>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field disabled label="First name" v-model="accountData.firstName"/>
+              </v-col>
+              <v-col cols="2">
+                <v-text-field disabled label="Prefix" v-model="accountData.prefix"/>
+              </v-col>
+              <v-col cols="4">
+                <v-text-field disabled label="Last name" v-model="accountData.lastName"/>
+              </v-col>
+            </v-row>
 
-          <v-text-field :readonly="editing?null:true" label="gender" v-model="accountData.gender"/>
-          <v-text-field :readonly="editing?null:true" label="street" v-model="accountData.street"/>
-          <v-row>
-            <v-col>
-              <v-text-field :readonly="editing?null:true" label="country" v-model="accountData.country"/>
-            </v-col>
-            <v-col>
-              <v-text-field :readonly="editing?null:true" label="nationality" v-model="accountData.nationality"/>
-            </v-col>
-          </v-row>
-          <v-text-field :readonly="editing?null:true" label="study" v-model="accountData.study"/>
-          <v-checkbox disabled label="Paid contribution?" v-model="accountData.contributionPaid"/>
-          <!--          TODO: dit leuk maken-->
-          <!--          <v-text-field :readonly="editing?null:true" label="Account created on" v-model="accountData.createdAt"/>-->
-          <!--          <v-text-field :readonly="editing?null:true" label="memberSince" v-model="accountData.memberSince"/>-->
+            <v-text-field disabled label="E-mail" v-model="accountData.email"/>
+            <v-text-field label="Address" v-model="accountData.address"/>
+            <v-row>
+              <v-col cols="10">
+                <v-text-field label="City" v-model="accountData.city"/>
+              </v-col>
+              <v-col cols="2">
+                <v-text-field label="Postal code" v-model="accountData.postalCode"/>
+              </v-col>
+            </v-row>
+            <v-text-field label="Phone number" v-model="accountData.phoneNumber"
+                          :rules="[v => !v || v.match(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/) || 'Fill in a correct phone number']"/>
+            <v-text-field label="Student number" v-model="accountData.studentNumber"/>
+            <v-text-field label="Date of birth" v-model="accountData.dateOfBirth" type="date"/>
+            <v-text-field label="Discord tag" v-model="accountData.discord"
+                          :rules="[v => !v || v.match(/^.{3,32}#[0-9]{4}$/) || 'Fill in a correct discord tag (maybe you forgot the numbers?)']"/>
+            <v-text-field label="Steam ID" v-model="accountData.steamid"/>
+            <v-row>
+              <v-col>
+                <v-checkbox label="Subscribe to newsletter"
+                            v-model="accountData.newsletter"/>
+              </v-col>
+              <v-col>
+                <v-checkbox label="Give consent for your photo to be taken at events"
+                            v-model="accountData.photoConsent"/>
+              </v-col>
+            </v-row>
+
+            <v-text-field label="Gender" v-model="accountData.gender"/>
+            <v-row>
+              <v-col>
+                <v-text-field label="Country" v-model="accountData.country"/>
+              </v-col>
+              <v-col>
+                <v-text-field label="Nationality" v-model="accountData.nationality"/>
+              </v-col>
+            </v-row>
+            <v-text-field label="Study" v-model="accountData.study"/>
+            <!-- TODO: do something with this-->
+            <!--          <v-checkbox disabled label="Paid contribution?" v-model="accountData.contributionPaid"/>-->
+
+          </v-form>
         </div>
 
 
@@ -91,29 +97,22 @@ export default {
   data: () => ({
     accountData: null,
     oldAccountData: null,
-    editing: false
+    valid: true
   }),
   methods: {
-    edit() {
-      this.oldAccountData = JSON.parse(JSON.stringify(this.accountData));
-      this.editing = true;
-    },
-    cancel() {
-      this.accountData = this.oldAccountData;
-      this.editing = false;
+    copyObject(obj) {
+      return Object.assign({}, obj);
     },
     save() {
-
-      //Send new user object to backend
-      const login = this.$store.getters.getLogin
-      this.$http
-          .put(`users/${login.userId}`, this.accountData, {headers: {'Authorization': `Bearer ${login.token}`}})
-          .then(() => {
-            this.editing = false;
-
-          }).catch(
-
-      );
+      if (this.$refs.form.validate()) {
+        //Send new user object to backend
+        const login = this.$store.getters.getLogin
+        this.$http
+            .put(`users/${login.userId}`, this.accountData, {headers: {'Authorization': `Bearer ${login.token}`}})
+            .then(() => {
+              this.editing = false;
+            }).catch();
+      }
     },
   },
   mounted() {
@@ -123,7 +122,8 @@ export default {
         .get(`users/${login.userId}`, {headers: {'Authorization': `Bearer ${login.token}`}})
         .then(response => {
           console.log(response.data)
-          return this.accountData = response.data
+          this.accountData = response.data
+          this.oldAccountData = this.copyObject(this.accountData)
         })
   }
 }
