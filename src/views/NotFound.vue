d19bcae8912e2b1688222107fb0c7f92b91f63fc
Peter Smit <p.j.m.smit@student.utwente.nl>
2022-09-15 15:48:36 +0200

    fucky wucky fix

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,11 +3,11 @@
     <div class="body-1 mx-auto mt-16 text-center" style="max-width: 800px;">
       <v-img class="mx-auto" :src="require('../assets/shellyooooooooooooohhhhhhhhhhh.png')"
              max-width="800"/>
-      <h3 class="mt-10">
+      <p class="mt-10 text-h6 text-sm-h5">
         Uh oh, we made a fucky wucky! Pwease don't report this incident to board otherwise they will
         whip us again. Or do if you're hate sitecie (but then we hate you too >:(
-      </h3>
-      <h1 class="mt-5">Click <a href="/" class="text-decoration-none">here</a> to go back to main page!</h1>
+      </p>
+      <p class="mt-5 text-h5 text-sm-h3">Click <a href="/" class="text-decoration-none">here</a> to go back to the main page!</p>
     </div>
   </v-main>
 </template>
