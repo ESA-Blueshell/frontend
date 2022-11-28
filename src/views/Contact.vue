ea73883de9ad90d18ad5c2eac4c111a472fb78b9
Mateusz Bartnicki <bartnickimateuszgame@gmail.com>
2022-11-28 19:45:59 +0100

    Contact Page Updated to post corona

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,14 +10,15 @@
           <router-link to="/membership/" class="text-decoration-none">Join us!</router-link>
           page. If you are just looking for
           more information, you can contact the board at
-          <a href="mailto:board@blueshell.utwente.nl" target="_blank" class="text-decoration-none">board@blueshell.utwente.nl</a> or join the
-          <a href="https://discord.gg/WyDhbAP" target="_blank" class="text-decoration-none">Discord</a> and send a PM to the
+          <a href="mailto:board@blueshell.utwente.nl" target="_blank" class="text-decoration-none">board@blueshell.utwente.nl</a>
+          or join the
+          <a href="https://discord.gg/WyDhbAP" target="_blank" class="text-decoration-none">Discord</a> and send a PM to
+          the
           board members.
         </p>
         <p class="body-1">
           Do you want to ask your question in person? Usually, you can find a board member in the
-          Blueshell room (Bastille 313) during the break.
-          <i>Due to the Coronavirus, the Blueshell room is closed until further notice.</i>
+          Predator Esports Lounge during the break.
         </p>
         <v-row>
           <v-col style="min-width: 200px">
@@ -31,11 +32,9 @@
           </v-col>
           <v-col>
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.002597906819!2d6.8497361159970485!3d52.24333646444751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b813d9fcd72811%3A0x9b5610e04ea3201c!2sBastille%2C%20De%20Hems%2010%2C%207522%20NL%20Enschede!5e0!3m2!1sen!2snl!4v1634068865309!5m2!1sen!2snl"
-                width="600" height="450" style="border:0;" allowfullscreen=""
-                loading="lazy"
-                v-bind:style="{filter: $vuetify.theme.dark ? 'invert(90%)' : ''}"
-            ></iframe>
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.009787256024!2d6.849677851362431!3d52.24320587966339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8133b297e5b15%3A0x918c3341c5447636!2sPredator%20Esports%20Lounge!5e0!3m2!1sen!2snl!4v1669661043450!5m2!1sen!2snl"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
           </v-col>
         </v-row>
 
