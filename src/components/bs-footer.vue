73f3a3abdd8d2f85a0676793ded4ccc80b62dfd8
Peter Smit <peter@elnino.tech>
2024-03-16 16:57:13 +0100

    we do a little testing

--- src/components/bs-footer.vue
+++ src/components/bs-footer.vue
@@ -49,17 +49,56 @@ const textLinks = [
 </script>
 
 <template>
-  <v-footer
-    v-if="$vuetify.display.mdAndUp"
-    theme="dark"
-    height="160"
-    class="pa-8 justify-center"
-  >
-    <v-row
-      align="center"
-      style="max-width: 1000px"
+  <v-lazy>
+    <v-footer
+      v-if="$vuetify.display.mdAndUp"
+      theme="dark"
+      height="160"
+      class="pa-8 justify-center"
     >
-      <div style="display: flex; flex-flow: column wrap; max-height: 120px; width: calc(3 * 48px);">
+      <v-row
+        align="center"
+        style="max-width: 1000px"
+      >
+        <div style="display: flex; flex-flow: column wrap; max-height: 120px; width: calc(3 * 48px);">
+          <v-btn
+            v-for="link in iconLinks"
+            :key="link.href"
+            :href="link.href"
+            target="_blank"
+            variant="plain"
+            :icon="link.icon"
+          />
+        </div>
+
+        <div
+          class="ml-8"
+          style="display: flex; flex-flow: column wrap; max-height: calc(2*36px + 4px);gap: 4px"
+        >
+          <v-btn
+            v-for="link in textLinks"
+            :key="link.href"
+            :href="link.href"
+            target="_blank"
+            v-text="link.text"
+          />
+        </div>
+
+        <v-spacer />
+
+        <div>
+          SITECIE GANG &copy; {{ new Date().getFullYear() }}
+        </div>
+      </v-row>
+    </v-footer>
+
+    <v-footer
+      v-else-if="$vuetify.display.smAndDown"
+      theme="dark"
+      class="d-flex flex-column pa-4"
+      :height="$vuetify.display.sm ? 216 : 304"
+    >
+      <div>
         <v-btn
           v-for="link in iconLinks"
           :key="link.href"
@@ -71,8 +110,9 @@ const textLinks = [
       </div>
 
       <div
-        class="ml-8"
-        style="display: flex; flex-flow: column wrap; max-height: calc(2*36px + 4px);gap: 4px"
+        class="mt-4"
+        :style="{ display: $vuetify.display.xs ? 'flex' : 'grid' }"
+        style="flex-direction: column; grid-template-columns: repeat(2, 1fr); gap: 8px;"
       >
         <v-btn
           v-for="link in textLinks"
@@ -83,49 +123,10 @@ const textLinks = [
         />
       </div>
 
-      <v-spacer />
 
-      <div>
+      <div class="mt-4">
         SITECIE GANG &copy; {{ new Date().getFullYear() }}
       </div>
-    </v-row>
-  </v-footer>
-
-
-  <v-footer
-    v-else-if="$vuetify.display.smAndDown"
-    theme="dark"
-    class="d-flex flex-column pa-4"
-    :height="$vuetify.display.sm ? 216 : 304"
-  >
-    <div>
-      <v-btn
-        v-for="link in iconLinks"
-        :key="link.href"
-        :href="link.href"
-        target="_blank"
-        variant="plain"
-        :icon="link.icon"
-      />
-    </div>
-
-    <div
-      class="mt-4"
-      :style="{ display: $vuetify.display.xs ? 'flex' : 'grid' }"
-      style="flex-direction: column; grid-template-columns: repeat(2, 1fr); gap: 8px;"
-    >
-      <v-btn
-        v-for="link in textLinks"
-        :key="link.href"
-        :href="link.href"
-        target="_blank"
-        v-text="link.text"
-      />
-    </div>
-
-
-    <div class="mt-4">
-      SITECIE GANG &copy; {{ new Date().getFullYear() }}
-    </div>
-  </v-footer>
+    </v-footer>
+  </v-lazy>
 </template>
